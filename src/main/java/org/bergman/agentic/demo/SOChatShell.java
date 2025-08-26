d8401f3e74dd9caa14bf75db8857df0ac267b5b4
ChristofferBergman <121811913+ChristofferBergman@users.noreply.github.com>
2025-08-26 15:12:30 +0200

    Moved system prompt
    Moved the system prompt, i.e. the agent description, from the @ShellComponent annotation to being part of the generateText() call.

--- src/main/java/org/bergman/agentic/demo/SOChatShell.java
+++ src/main/java/org/bergman/agentic/demo/SOChatShell.java
@@ -1,6 +1,7 @@
 package org.bergman.agentic.demo;
 
 import com.embabel.agent.api.common.Ai;
+import com.embabel.common.ai.model.LlmOptions;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -8,18 +9,18 @@ import org.springframework.shell.standard.ShellMethod;
 @ShellComponent
 record SOChatShell(Ai ai, SOChatAgentEmbabel agent) {
 
-    @ShellMethod(
-    		"""
-    		You are assisting a development team with questions on their specific development environment.
-    		For this you have a graph which is an export from Stack Overflow for teams. It has posts and comments on those posts.
-    		The original post is usually a question, and the other posts are answers on that question.
-    		All posts and comments has a link to the user that posted them.
-    		"""
-    		)
+    @ShellMethod
     public String answer(String userQuestion) {
         return ai
-                .withDefaultLlm()
+                .withLlm(LlmOptions.withDefaultLlm().withTemperature(.8))
                 .withToolObject(agent)
-                .generateText(userQuestion);
+                .generateText(
+                		"""
+                		You are assisting a development team with questions on their specific development environment.
+                		For this you have a graph which is an export from Stack Overflow for teams. It has posts and comments on those posts.
+                		The original post is usually a question, and the other posts are answers on that question.
+                		All posts and comments has a link to the user that posted them. The question is:
+                		""" + userQuestion
+                		);
     }
 }
